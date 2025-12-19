5dc4f3b6e27cdd24540676d20838816cf9ff8137
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-12-19 12:19:54 +0100

    editar primer count y btn en react native

--- 03-counter-app/App.tsx
+++ 03-counter-app/App.tsx
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [count, setCount] = useState(10)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!!!!!!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.textHuge}>{count}</Text>
+      <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+      >
+        <Text style={{ color: 'white', fontSize: 20 }}>+1</Text>
+      </Pressable>
+
+      {/* <TouchableOpacity></TouchableOpacity> para hacer un fade al tocar */}
+      <StatusBar style="dark" />
     </View>
   );
 }
@@ -17,4 +29,23 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textHuge: {
+    fontSize: 120,
+    fontWeight: '100'
+  },
+
+  floatingButton: {
+    position: 'absolute',
+    bottom: 20,
+    right: 20,
+    backgroundColor: '#65558F',
+    padding: 20,
+    borderRadius: 14,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    elevation: 3, /* añade una sombra en android */
+    shadowRadius: 4
+  }
 });
