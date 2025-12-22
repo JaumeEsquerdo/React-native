d6904280540eaae412a80ca7a1b0791073cbbe2d
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-12-22 19:49:51 +0100

    comentar funciones hook useCalculator.tsx

--- 04-calculator-app/hooks/useCalculator.tsx
+++ 04-calculator-app/hooks/useCalculator.tsx
@@ -9,11 +9,13 @@ enum Operator {
 }
 
 export const useCalculator = () => {
-    const [formula, setFormula] = useState('0')
+    const [formula, setFormula] = useState('0') // guarda la fórmula completa
 
-    const [number, setNumber] = useState('0')
-    const [prevNumber, setPrevNumber] = useState('0')
+    const [number, setNumber] = useState('0') // num actual q el user escribe
+    const [prevNumber, setPrevNumber] = useState('0') //guarda el resultado parcial, ref visual
 
+
+    // Si hay una operación, coloca el número después de ella; si no, la fórmula es solo el número actual
     useEffect(() => {
 
         if (lastOperation.current) {
@@ -26,26 +28,32 @@ export const useCalculator = () => {
 
     }, [number])
 
+    // Resultado parcial de la calculadora (ej: al escribir "500 +" se guarda y muestra el 500 debajo)
     useEffect(() => {
-        //todo: calcular subResultado
+        //calcula el resultado parcial
         const subResult = calculateSubResult()
         setPrevNumber(`${subResult}`)
     }, [formula])
 
+    //guarda la ultima operacion y no provoca render
     const lastOperation = useRef<Operator | undefined>(undefined)
 
+    // Calcula el resultado actual antes de guardar el número y empezar a escribir el siguiente
     const setLastNumber = () => {
         calculateResult()
 
+        // Evita números inválidos como "5."
         if (number.endsWith('.')) {
             setPrevNumber(number.slice(0, -1))
         }
-        setPrevNumber(number)
-        setNumber('0')
 
+        setPrevNumber(number)
+        //reinicia el num para escribir el siguiente
+        setNumber('0')
     }
 
     const divideOperation = () => {
+        //guarda num actual y define la operacion seleccionada
         setLastNumber()
         lastOperation.current = Operator.divide;
 
@@ -71,6 +79,7 @@ export const useCalculator = () => {
         const num1 = Number(firstValue);
         const num2 = Number(secondValue)
 
+        //si no hay segundo num, devuelve el primero solo
         if (isNaN(num2)) return num1;
 
         switch (operation) {
@@ -92,6 +101,7 @@ export const useCalculator = () => {
         const result = calculateSubResult()
         setFormula(`${result}`)
 
+        //limpia el estado de la operación después de calcular y mostrar resultado
         lastOperation.current = undefined;
         setPrevNumber('0')
     }
@@ -100,6 +110,7 @@ export const useCalculator = () => {
         //veriricar si ya existe un punto decimal
         if (number.includes('.') && numberString === '.') return
 
+        // Controla ceros iniciales
         if (number.startsWith('0') || number.startsWith('-0')) {
             if (numberString === '.') {
                 return setNumber(number + numberString)
@@ -122,6 +133,7 @@ export const useCalculator = () => {
         setNumber(number + numberString)
     }
 
+    // Resetea toda la calculadora
     const clean = () => {
         setNumber('0')
         setPrevNumber('0')
@@ -130,7 +142,7 @@ export const useCalculator = () => {
         lastOperation.current = undefined
     }
 
-
+    // toggle de signo negativo/positivo
     const toggleSign = () => {
         if (number.includes('-')) {
             return setNumber(number.replace('-', ''))
@@ -141,11 +153,13 @@ export const useCalculator = () => {
     }
 
     const deleteLast = () => {
+        // guarda el signo y el num
         let currentSign = '';
         let temporalNumber = number;
 
+
         if (number.includes('-')) {
-            currentSign = '-'; // guarda el signo '-', para despues aplicar la logica de si emporalNumber.length > 1, asi entran todos los numeros q son sean 0 
+            currentSign = '-'; // guarda el signo '-', para despues aplicar la logica de si emporalNumber.length > 1, asi entran todos los numeros q son sean 0 y 1 (para poder borrar el ultim num)
             temporalNumber = number.substring(1) //extrae una cadena  de number desde la posicion 1 hasta el final
         }
 
@@ -153,7 +167,7 @@ export const useCalculator = () => {
             return setNumber(currentSign + temporalNumber.slice(0, -1)) // quita el último número, conserva el - si existe y finaliza el método.
         }
 
-        setNumber('0') // si solo quedaba un digito, pone el 0
+        setNumber('0') // si solo quedaba un digito, pone el 0 directamente
     }
 
     return {
