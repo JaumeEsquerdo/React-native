a1e8df2efd0edf282ee63f5c6bf1a82898c3ba61
Jaume Esquerdo <jaume.esquerdo@alumnos.cei.es>
2025-12-29 12:14:15 +0100

    instalar nativewind en navigation-app

---
+++ 05-navigation-app/app/_layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./global.css"
+import { Slot } from 'expo-router'
+
+
+
+const RootLayout = () => {
+    return (
+        <Slot />
+    )
+}
+
+export default RootLayout
